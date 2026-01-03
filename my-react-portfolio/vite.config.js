bdeb5a42a112e052ff4d8ebf2f7010742c4230d6
Kato930 <katoooooo77@gmail.com>
2026-01-03 21:49:16 +0800

    commit

--- my-react-portfolio/vite.config.js
+++ my-react-portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Catipon-Midterm-Portfolio/", 
+  base: '/Catipon-Midterm-Portfolio/',
 })
