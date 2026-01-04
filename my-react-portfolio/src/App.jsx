23a3d8d2e8e3ce8bee58d2d85c0191bed41ed6ab
Kato930 <katoooooo77@gmail.com>
2026-01-04 17:12:11 +0800

    fix

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -19,15 +19,18 @@ function App() {
   }, [isDark]);
 
   return (
+    /* We are using HashRouter because it is 100% stable on GitHub Pages */
     <Router>
       <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
         <Navbar isDark={isDark} setIsDark={setIsDark} />
         
         <main className="flex-grow">
           <Routes>
+            {/* The default path "/" will now load Home automatically */}
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
+            {/* Redirects any errors back to Home */}
             <Route path="*" element={<Home />} />
           </Routes>
         </main>
