942c50621cae94fbec4e4f1812fa73e04a7b6420
Kato930 <katoooooo77@gmail.com>
2026-01-05 00:04:09 +0800

    FIX

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import './App.css';
-
-function App() {
-  const [isDark, setIsDark] = useState(false);
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [isDark]);
-
-  return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-        <Navbar isDark={isDark} setIsDark={setIsDark} />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-export default App;
+<Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/projects" element={<Projects />} />
+    {/* This line below catches everything and forces it Home if it gets lost */}
+    <Route path="*" element={<Home />} /> 
+  </Routes>
+</Router>
