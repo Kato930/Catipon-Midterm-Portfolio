05983fd594cfdaf1baa057d25f6d78655ac80c73
Kato930 <katoooooo77@gmail.com>
2026-01-04 15:01:18 +0800

    finnished

--- my-react-portfolio/src/components/Footer.jsx
+++ my-react-portfolio/src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-center p-3 mt-8 bg-red-300 text-white">
-      <p>&copy; 2025 Jon's Page. All rights reserved.</p>
+    <footer className="w-full py-6 mt-auto bg-red-100 border-t border-red-200">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-600 text-sm font-medium">
+          © 2025 Jon Catipon. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 };
